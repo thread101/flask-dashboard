e5e42d611fde815d34cb1f817675b460dc0f441d
thread101 <localthread101@gmail.com>
2026-01-14 19:00:02 +0300

    edited dict keys and getting querry

--- app.py
+++ app.py
@@ -3,21 +3,14 @@ import requests
 app=Flask(__name__)
 @app.route("/")
 def dashboard():
-<<<<<<< HEAD
-    return render_template("app.html")
-@app.route("/weather",methods=["POST"])
-=======
     # return "Welcome to our webpage"
     return render_template("app.html")
 @app.route("/weather", methods=["GET"])
->>>>>>> fec0eb9930639f466ae0743be0dacf5acce6ec9b
 def weather():
-    city = request.form.get("city")
+    #city = request.form.get("city")
     api_key = "c1ee229ff0b96db89a52ea6fe44a2adb"
     url = "https://api.openweathermap.org/data/2.5/weather"
-
-    if request.method.upper() == "GET":
-        city = request.form.get("city")
+    city = request.args.get("city")
 
     params = {
         "q": city,
@@ -27,6 +20,6 @@ def weather():
 
     response = requests.get(url, params=params)
     data = response.json()
-
+    response.close()
     
     return data
