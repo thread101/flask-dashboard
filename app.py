d45f6033164fc8c00e048d751c82a8ab2364211a
thread101 <localthread101@gmail.com>
2026-01-14 19:13:37 +0300

    added city capitalization

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 from flask import Flask, request, render_template
 import requests
-app=Flask(__name__)
+app=Flask(__file__)
+
 @app.route("/")
 def dashboard():
     # return "Welcome to our webpage"
     return render_template("app.html")
+
 @app.route("/weather", methods=["GET"])
 def weather():
     #city = request.form.get("city")
     api_key = "c1ee229ff0b96db89a52ea6fe44a2adb"
     url = "https://api.openweathermap.org/data/2.5/weather"
-    city = request.args.get("city")
+    city = request.args.get("city").capitalize()
 
     params = {
         "q": city,
