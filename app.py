fec0eb9930639f466ae0743be0dacf5acce6ec9b
thread101 <localthread101@gmail.com>
2026-01-14 17:27:13 +0300

    made changes on html and route params

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import requests
 app=Flask(__name__)
 @app.route("/")
 def dashboard():
-    return "Welcome to our webpage"
-@app.route("/weather")
+    # return "Welcome to our webpage"
+    return render_template("app.html")
+@app.route("/weather", methods=["GET"])
 def weather():
     #city = request.form.get("city")
     city="Nairobi"
     api_key = "c1ee229ff0b96db89a52ea6fe44a2adb"
     url = "https://api.openweathermap.org/data/2.5/weather"
+
+    if request.method.upper() == "GET":
+        city = request.form.get("city")
 
     params = {
         "city": city,
@@ -17,8 +21,9 @@ def weather():
         "units": "metric"
     }
 
+    url = ""
     response = requests.get(url, params=params)
     data = response.json()
 
     
-    return data    
+    return data
